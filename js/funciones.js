55967d179e6739745380ef6a1511927b90688fa2
danigiofernandez <fernandezdanielricardo@gmail.com>
2023-01-12 21:03:01 -0300

    funcionesDOM

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+document.getElementById("buttonFace").addEventListener("click" , function () {
+    document.getElementById("facebook").style.display = "inline";
+})
+document.getElementById("buttonInsta").addEventListener("click" , function () {
+    document.getElementById("instagram").style.display = "inline";
+})
+document.getElementById("buttonGit").addEventListener("click" , function () {
+    document.getElementById("github").style.display = "inline";
+})
---
+++ js/funcionesDatos.js
@@ -0,0 +1,9 @@
+document.getElementById("buttonEmail").addEventListener("click" , function () {
+    document.getElementById("email").style.display = "inline";
+})
+document.getElementById("buttonDire").addEventListener("click" , function () {
+    document.getElementById("direccion").style.display = "inline";
+})
+document.getElementById("buttonTel").addEventListener("click" , function () {
+    document.getElementById("telefono").style.display = "inline";
+})
